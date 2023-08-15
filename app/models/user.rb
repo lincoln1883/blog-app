4fcc79d4d97d8971bac89362688fd38a124ec01e
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-15 19:32:40 -0300

    Add method to update post counter.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments, through: :posts
   has_many :likes, through: :posts
+
+  after_commit :update_posts_counter, if: :saved_change_to_id?
+
+  private
+
+  def update_posts_counter
+    update(posts_counter: posts.count) unless posts.empty?
+  end
 end
