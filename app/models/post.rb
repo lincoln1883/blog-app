b4068655199227879caaa14560a94cbf6a6fa795
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-16 00:09:12 -0300

    Add validation for title, likes and comments counters.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes
 
+  validates :title, presence: true, length: { minimum: 3, maximum: 250 }
+  validates :likes_counter, type: Integer, comparison: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, type: Integer, comparison: { greater_than_or_equal_to: 0 }
+
   after_save :update_counters, if: :saved_change_to_id?
   after_create :increment_posts_counter
   after_destroy :decrement_posts_counter
