a2a1521ac252445e959299a7614f5d8c5ac669eb
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-17 13:28:24 -0300

    Remove decrement counter and callback from comment.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  belongs_to :post
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
   after_create :increment_comments_counter
-  after_destroy :decrement_comments_counter
-
-  private
 
   def increment_comments_counter
     post.increment!(:comments_counter)
-  end
-
-  def decrement_comments_counter
-    post.decrement!(:comments_counter)
   end
 end
