6f5342096d8719bdad47e0cab8b063723176205d
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-17 13:25:41 -0300

    Remove update counter method from user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,7 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { integer: true, greater_than_or_equal_to: 0 }
   validates :bio, presence: true, length: { minimum: 5, maximum: 500 }
 
-  after_commit :update_posts_counter, if: :saved_change_to_id?
-
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
-  end
-
-  private
-
-  def update_posts_counter
-    update(posts_counter: posts.count)
   end
 end
