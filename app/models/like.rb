9a8fa36a3357f00181e8b964e643d17c4e43ab5d
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-17 13:28:47 -0300

    Update association.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   after_create :increment_likes_counter
