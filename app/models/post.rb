8549cd389ec86ab3b24907c90328554d04974c28
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-17 13:27:34 -0300

    Remove decrement counter and callbacks from post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,17 +4,11 @@ class Post < ApplicationRecord
   has_many :likes
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :text, presence: true, length: { minimum: 10, maximum: 250 }
+  validates :text, presence: true, length: { minimum: 5, maximum: 250 }
   validates :likes_counter, numericality: { integer: true, greater_than_or_equal_to: 0 }
   validates :comments_counter, numericality: { integer: true, greater_than_or_equal_to: 0 }
 
-  after_save :update_counters
   after_create :increment_posts_counter
-  after_destroy :decrement_posts_counter
-
-  def update_counters
-    update(comments_counter: comments.count, likes_counter: likes.count)
-  end
 
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
@@ -22,13 +16,7 @@ class Post < ApplicationRecord
 
   private
 
-  def decrement_posts_counter
-    author.decrement(:posts_counter)
-    author.save
-  end
-
   def increment_posts_counter
     author.increment(:posts_counter)
-    author.save
   end
 end
