ab7d9866e9728cdd352d6354731091eee3689a4a
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-21 12:42:11 -0300

    Create users posts route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/users/:id/posts', to: 'posts#index'
   get '/user/:id', to: 'users#show'
   # Defines the root path route ("/")
   root "users#index"
