29bdb3e85910ad4e7dbc5fb48d1581a6fa45c787
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-21 23:12:04 -0300

    Add user show test for users-request-spec file.

--- spec/requests/users_requests_spec.rb
+++ spec/requests/users_requests_spec.rb
@@ -2,9 +2,35 @@ require 'rails_helper'
 
 RSpec.describe User, type: :request do
   describe 'Get /index' do
-    it 'returns success' do
-      get '/users/index'
+    it '#returns success' do
+      get '/'
       expect(response.status).to eq(200)
+      expect(response).to have_http_status(:success)
+    end
+    it '#renders the index page' do
+      get '/'
+      expect(response).to render_template(:index)
+    end
+    it '#render the html body' do
+      get '/'
+      expect(response.body).to include('Users index')
+    end
+  end
+  describe 'Get /show' do
+    it '#returns success' do
+      get '/users/:id'
+      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:success)
+    end
+    it '#returns the user show page' do
+      id = 1
+      get "/users/#{id}"
+      expect(response).to render_template(:show)
+    end
+    it '#renders the body' do
+      id = 1
+      get "/users/#{id}"
+      expect(response.body).to include('User Show Page')
     end
   end
 end
