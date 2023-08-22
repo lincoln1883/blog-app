1d1eae549a582becf766e83b6c633db426d5a346
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-22 20:35:16 -0300

    Add error handling.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,7 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:author_id])
     @post = @user.posts.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
