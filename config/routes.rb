5e5d534f5d9be24b9fe69053f2190af820fb21f0
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-22 23:16:29 -0300

    Create the new post route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/users/:id/posts', to: 'posts#index'
+  get '/users/:author_id/posts/new', to: 'posts#new', as: :post
   get '/users/:author_id/posts/:id', to: 'posts#show'
   get '/users/:id', to: 'users#show'
   root 'users#index'
