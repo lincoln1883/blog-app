76e21f3e7171a83b2a7f8992a4eaead4915b2eae
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-23 18:13:18 -0300

    Edit post id in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   post '/users/:author_id/posts', to: 'posts#create', as: :user_posts
   get '/users/:author_id/posts/new', to: 'posts#new', as: :current_user_posts
-  get '/users/:author_id/posts/:id', to: 'posts#show'
+  get '/users/:author_id/posts/:post_id', to: 'posts#show'
   get '/users/:author_id/posts/:post_id/comments/new', to: 'comments#new', as: :new_author_post_comments
   post '/users/:author_id/posts/:post_id/comments', to: 'comments#create', as: :user_post_comments
   get '/users/:id', to: 'users#show'
