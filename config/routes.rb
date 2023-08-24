d9683ec0d5db3b075552b360fee21e09025a119f
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-24 13:24:07 -0300

    Add custom routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get '/users/:author_id/posts/:post_id/comments/new', to: 'comments#new', as: :new_author_post_comments
   post '/users/:author_id/posts/:post_id/comments', to: 'comments#create', as: :user_post_comments
   post '/users/:author_id/posts/:post_id/likes', to: 'likes#create', as: :user_post_likes
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/posts', to: 'posts#index'
+  get '/users/:id', to: 'users#show', as: :user_views
+  get '/users/:id/posts', to: 'posts#index', as: :user_posts_views
 
   root  'users#index'
 end
