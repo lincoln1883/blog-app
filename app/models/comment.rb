3c374bec58a9cd702c1d886fb8c8f8e454d28115
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-24 13:22:32 -0300

    Add validation to comments model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
+  validates_presence_of :text, presence: true, length: { minimum: 3, maximum: 250 }
   after_create :increment_comments_counter
 
   def increment_comments_counter
