1f61d520779a0b40d06f9903dab06a4425bada1a
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-29 10:46:57 -0300

    Add includes method to load all comments when the posts loads.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.includes(:comments).find(params[:user_id])
   end
 
   def create
