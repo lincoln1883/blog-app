34e99acbd6c24600c9104f4ede61c67ab87f6a5b
cosmosha <cosmoshagan@gmail.com>
2023-08-29 23:16:28 +0000

    Add spec/system test show page

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -32,4 +32,44 @@ RSpec.describe 'Users/index', type: :system do
       expect(page).to have_content(user.name)
     end
   end
+
+  describe 'Show page' do
+    user = User.create(name: 'Test User', bio: 'it me again', photo: 'https://picsum.photos/200/300', posts_counter: 1)
+    let(:user) { create(:user, :post) }
+    let(:post) { create(:post, user:) }
+
+    before do
+      visit user_path(user)
+    end
+
+    it 'should display user photo' do
+      expect(page).to have_selector("img[src='#{user.photo}']")
+    end
+
+    it 'should display username' do
+      expect(page).to have_content(user.name)
+    end
+
+    it 'should display number of posts' do
+      expect(page).to have_content("number of posts: #{user.posts_counter}")
+    end
+
+    it 'should display Bio' do
+      expect(page).to have_content(user.bio)
+    end
+
+    it 'should display user\'s first 3 posts' do
+      user.posts.limit(3).each do |post|
+        expect(page).to have_content(post.title)
+      end
+    end
+
+    it 'should display button for viewing all user\'s posts' do
+      expect(page).to have_link('See all posts')
+    end
+
+    it 'should redirect to the user\'s post' do
+      expect(page).to have_link('See all posts')
+    end
+  end
 end
