8d5b87e5bfa04c92ec19fd66a031f920fdae7556
cosmosha <cosmoshagan@gmail.com>
2023-08-30 15:13:54 +0000

    Add posts_show test for author,comments count, like count, text, comments of commenters

--- spec/system/users_show_page_spec.rb
+++ spec/system/users_show_page_spec.rb
@@ -21,26 +21,5 @@ RSpec.describe 'Users/show', type: :system do
       expect(page).to have_content(user.name)
     end
 
-    it 'should display number of posts' do
-      expect(page).to have_content("number of posts: #{user.posts_counter}")
-    end
-
-    it 'should display Bio' do
-      expect(page).to have_content(user.bio)
-    end
-
-    it 'should display user\'s first 3 posts' do
-      user.posts.limit(3).each do |post|
-        expect(page).to have_content(post.title)
-      end
-    end
-
-    it 'should display button for viewing all user\'s posts' do
-      expect(page).to have_link('See all posts')
-    end
-
-    it 'should redirect to the user\'s post' do
-      expect(page).to have_link('See all posts')
-    end
   end
 end
