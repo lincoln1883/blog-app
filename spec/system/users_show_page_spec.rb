679407e08f53362131080364bc62ff8b789a5132
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-29 21:44:08 -0300

    Create spec file to test user post page.

--- spec/system/users_show_page_spec.rb
+++ spec/system/users_show_page_spec.rb
@@ -1,38 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'Users/index', type: :system do
+RSpec.describe 'Users/show', type: :system do
   before do
     driven_by(:rack_test)
   end
-  describe 'Index page' do
-    user = User.create(name: 'Test User', bio: 'it me again', photo: 'https://picsum.photos/200/300', posts_counter: 1)
-    it 'should display the Logo' do
-      visit root_path
-      expect(page).to have_content('BlogApp')
-    end
-    it 'should display username' do
-      visit '/users'
-      expect(page).to have_content(user.name)
-    end
-    it 'should display photo' do
-      visit '/users'
-      expect(page).to have_selector("img[src*='#{user.photo}']")
-    end
-    it 'should display number of posts' do
-      visit '/users'
-      expect(page).to have_content("number of posts: #{user.posts_counter}")
-    end
-    it 'should redirect to the user show page' do
-      user = User.create(name: 'Tommy', bio: 'it me again', photo: 'https://picsum.photos/200/300', posts_counter: 1)
-      visit '/users'
-      within '.card', text: user.name do
-        click_link 'See posts'
-      end
-      expect(current_path).to eq(user_path(user.id))
-      expect(page).to have_content(user.name)
-    end
-  end
-
   describe 'Show page' do
     user = User.create(name: 'Test User', bio: 'it me again', photo: 'https://picsum.photos/200/300', posts_counter: 1)
     let(:user) { create(:user, :post) }
