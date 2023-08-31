10cd6e352cbfbae8f67ffbead429a5d79bbdffc7
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-30 23:07:16 -0300

    Implemented changes requested in CR.

--- spec/system/users_index_pages_spec.rb
+++ spec/system/users_index_pages_spec.rb
@@ -5,23 +5,38 @@ RSpec.describe 'Users/Index', type: :system do
     driven_by(:rack_test)
   end
   describe 'Index page' do
-    user = User.create(name: 'Test User', bio: 'it me again', photo: 'https://picsum.photos/200/300', posts_counter: 1)
+    user = User.create(name: 'Test User', bio: 'it me again', photo: 'photo1.png', posts_counter: 1)
+    user2 = User.create(name: 'Test User2', bio: 'it me again', photo: 'photo2.png', posts_counter: 1)
+    user3 = User.create(name: 'Test User3', bio: 'it me again', photo: 'photo3.png', posts_counter: 1)
+    
     it 'I can see the username of all other users.' do
       user = User.all
       visit users_path
       expect(page).to have_content(user.name)
+      expect(page).to have_content(user2.name)
+      expect(page).to have_content(user3.name)
     end
+    
     it 'I can see the profile picture for each user.' do
       visit users_path
-      expect(page).to have_css("img[src*='#{user.photo}']")
+      expect(page).to have_css("img[src*='photo1.png']")
+      expect(page).to have_css("img[src*='photo2.png']")
+      expect(page).to have_css("img[src*='photo3.png']")
     end
 
     it 'I can see the number of posts for each user has written.' do
+    user = User.create(name: 'Test User', bio: 'it me again', photo: 'photo1.png', posts_counter: 1)
+      5.times do |i|
+          Post.create(author: user, text: "This is a test post number #{i}")
+        end
+      
       visit users_path
-      expect(page).to have_content(user.posts_counter)
+      
+      expect(page).to have_content("number of posts: #{user.posts_counter}")
+      save_and_open_page
     end
     it 'When I click on a user, I am redirected to that user show page' do
-      user = User.create(name: 'Tommy', bio: 'it me again', photo: 'https://picsum.photos/200/300', posts_counter: 1)
+      user = User.create(name: 'Tommy', bio: 'it me again', photo: 'photo1.png', posts_counter: 1)
       visit users_path
       within '.card', text: user.name do
         click_link 'See posts'
