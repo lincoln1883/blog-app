c009cab28798bdcf9c687ee87c06d2c5694b8110
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-31 11:38:16 -0300

    Add devise_for users routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root  'users#index'
+
   resources :users do
     resources :posts do
       resources :comments
       resources :likes
     end
   end
-  root  'users#index'
 end
