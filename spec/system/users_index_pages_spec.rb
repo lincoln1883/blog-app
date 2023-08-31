0f75b5cc68cfde1f1132f14479ff2e6c83658e3c
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-31 00:25:31 -0300

    Fix linter errors.

--- spec/system/users_index_pages_spec.rb
+++ spec/system/users_index_pages_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Users/Index', type: :system do
     user = User.create(name: 'Test User', bio: 'it me again', photo: 'photo1.png', posts_counter: 1)
     user2 = User.create(name: 'Test User2', bio: 'it me again', photo: 'photo2.png', posts_counter: 1)
     user3 = User.create(name: 'Test User3', bio: 'it me again', photo: 'photo3.png', posts_counter: 1)
-    
+
     it 'I can see the username of all other users.' do
       user = User.all
       visit users_path
@@ -16,7 +16,7 @@ RSpec.describe 'Users/Index', type: :system do
       expect(page).to have_content(user2.name)
       expect(page).to have_content(user3.name)
     end
-    
+
     it 'I can see the profile picture for each user.' do
       visit users_path
       expect(page).to have_css("img[src*='photo1.png']")
@@ -25,15 +25,14 @@ RSpec.describe 'Users/Index', type: :system do
     end
 
     it 'I can see the number of posts for each user has written.' do
-    user = User.create(name: 'Test User', bio: 'it me again', photo: 'photo1.png', posts_counter: 1)
+      user = User.create(name: 'Test User', bio: 'it me again', photo: 'photo1.png', posts_counter: 1)
       5.times do |i|
-          Post.create(author: user, text: "This is a test post number #{i}")
-        end
-      
+        Post.create(author: user, text: "This is a test post number #{i}")
+      end
+
       visit users_path
-      
+
       expect(page).to have_content("number of posts: #{user.posts_counter}")
-      save_and_open_page
     end
     it 'When I click on a user, I am redirected to that user show page' do
       user = User.create(name: 'Tommy', bio: 'it me again', photo: 'photo1.png', posts_counter: 1)
