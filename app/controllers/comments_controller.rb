a8bc2a057dad8e248e3c588ab829c1540d16e52c
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-01 20:01:25 -0300

    Fix linter errors.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
-  
+
   def new
     @user = current_user
     @post = Post.find(params[:post_id])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,22 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!  
+  before_action :authenticate_user!
   load_and_authorize_resource
 
   def index
     @user = User.includes(:comments).find(params[:user_id])
-    @user = User.includes(:posts).find(params[:user_id]) 
+    @user = User.includes(:posts).find(params[:user_id])
     @post = Post.includes(:author, :comments, :likes).where(author_id: @user.id)
   end
 
-  def create   
-     @post = Post.new(post_params)
-     @post.author = current_user    
-     if @post.save      
-      redirect_to user_path(current_user.id), notice: 'Post was successfully created'    
-     else      
-      flash[:alert] = 'There was an error saving this post.'      
-      render :new, status: :unprocessable_entity    
-     end  
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to user_path(current_user.id), notice: 'Post was successfully created'
+    else
+      flash[:alert] = 'There was an error saving this post.'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
