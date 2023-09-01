9077a3c5227da718aa40abbda3b01dbc364ce4e6
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-01 19:51:21 -0300

    Add dependent delete to post foreign key.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :posts, foreign_key: :author_id
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, through: :posts
   has_many :likes, through: :posts
 
