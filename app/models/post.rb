343c3a0c114d72ee10f39de2d241ced2757607a9
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-01 19:54:29 -0300

    Add decrement method and counter callback.

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { integer: true, greater_than_or_equal_to: 0 }
 
   after_save :increment_posts_counter
+  after_destroy :decrement_posts_counter
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
@@ -18,5 +19,9 @@ class Post < ApplicationRecord
 
   def increment_posts_counter
     author.increment!(:posts_counter)
+  end
+
+  def decrement_posts_counter
+    author.decrement!(:posts_counter)
   end
 end
