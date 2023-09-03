8d3233681934338d1c58ae399c50faef418213c6
Lincoln Gibson <lincolngibson7@gmail.com>
2023-09-03 20:33:36 -0300

    Modify the ability class permissions logic.
    Co-authored-by: cosmosha <cosmoshagan@gmail.com>

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
-    can :read, Post
-    if user.role == 'user'
-      can :create, Post
-      can :destroy, Post, author_id: user.id
-      can :create, Comment
-      can :destroy, Comment, author_id: user.id
-    elsif user.admin?
-      can :manage, :all
-    end
+    can :read, Post # start by defining rules for all users, also not logged ones
+    return unless user.present?
+
+    can :create, Post
+    can :destroy, Post, author_id: user.id # if the user is logged in can manage it's own posts
+    can :create, Comment # logged in users can also create comments
+    can :destroy, Comment, author_id: user.id
+    return unless user.admin?
+
+    can :manage, :all # finally we give all remaining permissions only to the admins
   end
 end
