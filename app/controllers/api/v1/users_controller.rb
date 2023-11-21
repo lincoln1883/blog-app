2a6d046dab1770722be999fc39dc5e44743386ed
Lincoln Gibson <lincolngibson7@gmail.com>
2023-11-21 20:46:51 -0300

    Remove whitespace.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,6 @@ module Api
     class UsersController < ApplicationController
       def index
         @user = User.all
-
         respond_to do |format|
           format.json { render json: @user }
         end
